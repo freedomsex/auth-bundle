ad52b4f4ddaf6c2ab29500d10bf2582db19ae855
tebaly <support@freedomsex.net>
2019-07-02 08:05:09 +0300

    feat: id service

---
+++ src/Services/ClientIdentifier.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class ClientIdentifier
+{
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function request()
+    {
+        return $this->requestStack->getCurrentRequest();
+    }
+
+    public function ip()
+    {
+        return $this->request()->getClientIp();
+    }
+
+    public function agent()
+    {
+        return $this->request()->headers->get('User-Agent');
+    }
+}
